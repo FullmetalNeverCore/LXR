0dff32f757d64c650ad373c7e61d17b433c05f71
Ilja <i.tsygankov@balticsolutions.lv>
2026-03-05 07:04:56 +0200

    LXR-Fix CORS

--- backend/main.py
+++ backend/main.py
@@ -9,6 +9,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://localhost:5174",
+        "https://lxr-five.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
